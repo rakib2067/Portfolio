f0bc5c4b62416d8db655b03f92909c4f52898a96
rakib2067 <rakib7@hotmail.co.uk>
2021-11-29 21:49:16 +0000

    added projects carousel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
 const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item)=>
-item.addEventListener("click", function(){
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
-})
+  })
 );
+
+let carouselPosition = 0;
+
+const projects = document.getElementsByClassName("carousel__item");
+const totalProjects = projects.length;
+
+document
+  .getElementById("carousel__button--next")
+  .addEventListener("click", function () {
+    moveToNextSlide();
+  });
+document
+  .getElementById("carousel__button--prev")
+  .addEventListener("click", function () {
+    moveToPrevSlide();
+  });
+
+function updateProjectPosition() {
+  for (let project of projects) {
+    project.classList.remove("carousel__item--visible");
+    project.classList.add("carousel__item--hidden");
+  }
+  projects[carouselPosition].classList.add("carousel__item--visible");
+}
+
+function moveToNextSlide() {
+  if (carouselPosition === totalProjects - 1) {
+    carouselPosition = 0;
+  } else {
+    carouselPosition++;
+  }
+  updateProjectPosition();
+}
+function moveToPrevSlide() {
+  if (carouselPosition === 0) {
+    carouselPosition = totalProjects - 1;
+  } else {
+    carouselPosition--;
+  }
+  updateProjectPosition();
+}
